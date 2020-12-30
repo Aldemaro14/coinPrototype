440d35d1e2314156c64843227254fa79dec2ef6c
kevintoloza <tolozakevin96@gmail.com>
2020-12-30 11:26:50 -0600

    Minor fixes

--- cetacoin/authentication/models.py
+++ cetacoin/authentication/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 
 class UserCrypto(models.Model):
+    username = models.CharField(max_length=15)
     firstName = models.CharField(max_length=254)
     lastName = models.CharField(max_length=254)
     email = models.EmailField(max_length=254)
--- cetacoin/authentication/serializers.py
+++ cetacoin/authentication/serializers.py
@@ -6,19 +6,22 @@ from authentication.models import UserCrypto
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=65, min_length=8, write_only=True)
     email = serializers.EmailField(max_length=255, min_length=4)
-    firstName = serializers.CharField(max_length=255, min_length=2)
-    lastName = serializers.CharField(max_length=255, min_length=2)
+    first_name = serializers.CharField(max_length=255, min_length=2)
+    last_name = serializers.CharField(max_length=255, min_length=2)
 
     class Meta:
-        model = UserCrypto
-        fields = ['firstName', 'lastName', 'email', 'password']
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+
 
     def validate(self, attrs):
         email = attrs.get('email', '')
-        if UserCrypto.objects.filter(email=email).exists():
+        if User.objects.filter(email=email).exists():
             raise serializers.ValidationError(
                 {'email': ('Email is already in use')})
         return super().validate(attrs)
+
+
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
