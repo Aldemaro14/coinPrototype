77f37566b8a48b821edf72d9bb0f0162278ed196
ErickQ06 <40754979+ErickQ06@users.noreply.github.com>
2020-12-30 09:23:15 -0600

    Adding Models, minor fixes in serializers.py

--- cetacoin/authentication/serializers.py
+++ cetacoin/authentication/serializers.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=65, min_length=8, write_only=True)
-    email = serializers.EmailField(max_length=255, min_length=4),
-    first_name = serializers.CharField(max_length=255, min_length=2)
-    last_name = serializers.CharField(max_length=255, min_length=2)
+    email = serializers.EmailField(max_length=255, min_length=4)
+    firstName = serializers.CharField(max_length=255, min_length=2)
+    lastName = serializers.CharField(max_length=255, min_length=2)
 
     class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        model = UserCrypto
+        fields = ['firstName', 'lastName', 'email', 'password']
 
     def validate(self, attrs):
         email = attrs.get('email', '')
-        if User.objects.filter(email=email).exists():
+        if UserCrypto.objects.filter(email=email).exists():
             raise serializers.ValidationError(
                 {'email': ('Email is already in use')})
         return super().validate(attrs)
@@ -25,8 +25,18 @@ class UserSerializer(serializers.ModelSerializer):
 
 class LoginSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=65, min_length=8, write_only=True)
-    username = serializers.CharField(max_length=255, min_length=2)
+    email = serializers.EmailField(max_length=255, min_length=4)
+    #username = serializers.CharField(max_length=255, min_length=2)
 
     class Meta:
-        model = User
-        fields = ['username', 'password']
+        model = UserCrypto
+        fields = ['email', 'password']
+
+        def validate(self, attrs):
+            email = attrs.get('email', '')
+            try:
+                UserCrypto.objects.filter(email=email).exist()
+            except UserCrypto.DoesNotExist:
+                raise serializers.ValidationError(
+            {'email' : ('Email does not exist')})
+            return super().validate(attrs)
